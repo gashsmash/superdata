144f3c2cdfbb4713efeeed03b0d8e7f51e125357
Silje Irene Hansen <siljeih@null13.math.ntnu.no>
2014-02-17 16:56:43 +0100

    MPI finnished! But a problem with n not divideble on all processors

--- prob4/task3.c
+++ prob4/task3.c
@@ -11,7 +11,7 @@
 
 // Initialize functions
 double *generateVectorV(int n);
-double computeSum(double *vector, int n, int rank, int size);
+double computeSum(double *vector, int n);
 double computeDifference(double sum, int n);
 // Initialize help-functions
 void printVector(double *vector, int n);
@@ -19,10 +19,10 @@ void printVector(double *vector, int n);
 
 int main (int argc, char **argv){
 	// Initialize variables
-	int n, k, kMax = 4;
-	double *v, sum, difference;
+	int n, k, kMax = 14;
+	double sum, difference, partial_sum;
 	// Variables used for MPI
-	int rank, size, tag, i;
+	int rank, size, tag, i, block_size;
 	MPI_Status status;
 	
 	MPI_Init(&argc, &argv);
@@ -30,29 +30,29 @@ int main (int argc, char **argv){
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
 	
-	k=3;
-//	for (k=3; k<kMax; k++){
+	for (k=3; k<kMax; k++){
+		n = pow(2,k);
+		block_size = n/size;
+		double *v = malloc(n*sizeof(double));
+		double *partial_v = malloc(block_size*sizeof(double));
+
 		if (rank == 0) {
-			n = pow(2,k); 			//gcc code.c -lm -o code
 			v = generateVectorV(n);
 		}
-		MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		double partial_sum = 0;
-		printf("0: %f \n1: %f \n2: %f \n3: %f\n4: %f\n5: %f",v[0],v[1],v[2],v[3],v[4],v[5]);
-		for (i=rank; i<n; i += size){
-			printf(" Proc %i have the value %f , and i is %i\n", rank, v[i],i);
-			partial_sum = partial_sum + v[i];
-		}
-//		printf("proc %i computed: %f \n", rank, (float)partial_sum);
+		
+		MPI_Scatter(v, block_size, MPI_DOUBLE, partial_v, block_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		partial_sum = computeSum(partial_v, block_size);
+		//printf("proc %i computed: %f \n", rank, partial_sum);
+		
+		//printf("%i: %f \t %f \n", rank, partial_v[0],partial_v[1]);
+		
 		MPI_Reduce(&partial_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-//		MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-//			sum = computeSum(v, n, rank, size);
-//		MPI_Reduce(sbuf,rbuf, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+		
 		if (rank == 0) {
 		difference = computeDifference(sum,n);
-		printf("\n\naiaiai %f \n",difference);
+		printf("Difference:  %f \n",difference);
 		}
-//	}
+	}
 	MPI_Finalize();
 
 }
@@ -65,24 +65,18 @@ double *generateVectorV(int n){
 	int i;
 	for (i=1; i<=n; i++){
 		v[i-1] = pow(i,-2);
-		printf("v element %i is %f \n",i-1, v[i-1]);
+		//printf("v element %i is %f \n",i-1, v[i-1]);
 	}
 	return v;
 }
 
-double computeSum(double *vector, int n, int rank, int size){
+double computeSum(double *vector, int n){
 	double sum = 0;
 	int i;
-	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	double partial_sum = 0;
-	for (i=rank; i<n; i += size){
-		partial_sum += vector[i];
+	for (i=0; i<n; i++){
+		sum += vector[i];
 	}
-	printf("proc %i computed: %f +n", rank, partial_sum);
-	MPI_Reduce(&partial_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-	if (rank == 0) {
-		return sum;
-	}
+	return sum;
 }
 
 double computeDifference(double sum, int n){
