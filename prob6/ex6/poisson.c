5150fb0ce00913ba59345dd625f39117fb4ece09
Silje Irene Hansen <siljeih@rocks.hpc.ntnu.no>
2014-04-01 15:34:15 +0200

    some comments in poisson.c

--- prob6/ex6/poisson.c
+++ prob6/ex6/poisson.c
@@ -39,11 +39,11 @@ int main(int argc, char **argv )
     return 1;
   }
 
-  n  = atoi(argv[1]);
-  m  = n-1;
-  nn = 4*n;
+  n  = atoi(argv[1]); 	// convert string to integer
+  m  = n-1;		// number of degrees of freedom in each spatial direction
+  nn = 4*n;		// ? 
 
-  diag = createRealArray (m);
+  diag = createRealArray (m); 		
   b    = createReal2DArray (m,m);
   bt   = createReal2DArray (m,m);
   z    = createRealArray (nn);
@@ -51,24 +51,32 @@ int main(int argc, char **argv )
   h    = 1./(Real)n;
   pi   = 4.*atan(1.);
 
+ // Make the diag vector
   for (i=0; i < m; i++) {
     diag[i] = 2.*(1.-cos((i+1)*pi/(Real)n));
   }
+ 
+ // Make the b matrix
   for (j=0; j < m; j++) {
     for (i=0; i < m; i++) {
       b[j][i] = h*h;
     }
   }
+ 
+ // fst??
   for (j=0; j < m; j++) {
     fst_(b[j], &n, z, &nn);
   }
 
+ //
   transpose (bt,b,m);
 
+ // fstinv??
   for (i=0; i < m; i++) {
     fstinv_(bt[i], &n, z, &nn);
   }
   
+ 
   for (j=0; j < m; j++) {
     for (i=0; i < m; i++) {
       bt[j][i] = bt[j][i]/(diag[i]+diag[j]);
@@ -101,13 +109,72 @@ void transpose (Real **bt, Real **b, int m)
   int i, j;
   for (j=0; j < m; j++) {
     for (i=0; i < m; i++) {
-      bt[j][i] = b[i][j];
+ /*
+  C-program to solve the two-dimensional Poisson equation on 
+  a unit square using one-dimensional eigenvalue decompositions
+  and fast sine transforms
+
+  einar m. ronquist
+  ntnu, october 2000
+  revised, october 2001
+*/
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
+#include <math.h>
+
+typedef double Real;
+
+/* function prototypes */
+Real *createRealArray (int n);
+Real **createReal2DArray (int m, int n);
+void transpose (Real **bt, Real **b, int m);
+void fst_(Real *v, int *n, Real *w, int *nn);
+void fstinv_(Real *v, int *n, Real *w, int *nn);
+
+
+int main(int argc, char **argv )
+{
+  Real *diag, **b, **bt, *z;
+  Real pi, h, umax;
+  int i, j, n, m, nn;
+
+  /* the total number of grid points in each spatial direction is (n+1) */
+  /* the total number of degrees-of-freedom in each spatial direction is (n-1) */
+  /* this version requires n to be a power of 2 */
+
+ if( argc < 2 ) {
+    printf("need a problem size\n");
+    return 1;
+  }
+
+  n  = atoi(argv[1]);   // convert string to integer
+  m  = n-1;             // number of degrees of freedom in each spatial direction
+  nn = 4*n;             // ? 
+
+  diag = createRealArray (m);
+  b    = createReal2DArray (m,m);
+  bt   = createReal2DArray (m,m);
+  z    = createRealArray (nn);
+
+  h    = 1./(Real)n;
+  pi   = 4.*atan(1.);
+
+ // Make the diag vector
+  for (i=0; i < m; i++) {
+    diag[i] = 2.*(1.-cos((i+1)*pi/(Real)n));
+  }
+
+ // Make the b matrix
+     bt[j][i] = b[i][j];
     }
   }
 }
 
-Real *createRealArray (int n)
-{
+// Creates 1D array with only 0.0 of Real type.
+Real *createRealArray (int n) {
   Real *a;
   int i;
   a = (Real *)malloc(n*sizeof(Real));
@@ -117,8 +184,8 @@ Real *createRealArray (int n)
   return (a);
 }
 
-Real **createReal2DArray (int n1, int n2)
-{
+// Creates 2D array with only 0.0 of Real type.
+Real **createReal2DArray (int n1, int n2) {
   int i, n;
   Real **a;
   a    = (Real **)malloc(n1   *sizeof(Real *));
